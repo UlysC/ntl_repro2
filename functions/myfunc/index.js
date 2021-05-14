0e5d195f416b956db70475f023064a31baa0cb07
UlysC <ulys.chambard@gmail.com>
2021-05-14 15:55:02 +0200

    rmv headers

--- functions/myfunc/index.js
+++ functions/myfunc/index.js
@@ -1,10 +1,6 @@
 exports.handler = async event => {
   return {
     statusCode: 200,
-    headers: JSON.stringify({
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true
-    }),
     body: JSON.stringify({ message: "hello" })
   }
 }

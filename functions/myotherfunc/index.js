946182a50c7a9126df4817b3b59c67e295b676fe
UlysC <ulys.chambard@gmail.com>
2021-07-06 09:25:36 +0200

    myotherfunc

--- functions/myotherfunc/index.js
+++ functions/myotherfunc/index.js
@@ -1,6 +1,6 @@
 exports.handler = async event => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "hello from myfunc" })
+    body: JSON.stringify({ message: "hello from my other func" }),
   }
 }

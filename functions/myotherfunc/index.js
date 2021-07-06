31c66ff962ea3cd0b98921d824c53be769eaf3f8
UlysC <ulys.chambard@gmail.com>
2021-07-06 09:29:22 +0200

    add headers

--- functions/myotherfunc/index.js
+++ functions/myotherfunc/index.js
@@ -1,6 +1,13 @@
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "POST, OPTION",
+  "Content-Type": "application/json",
+}
 exports.handler = async event => {
   return {
     statusCode: 200,
+    headers,
     body: JSON.stringify({ message: "hello from my other func" }),
   }
 }
